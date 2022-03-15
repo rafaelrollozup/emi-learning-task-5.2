593b9033bca06c450b439a21113d4316b74164da
Rafael Rollo <rafaelrollo92@gmail.com>
2022-03-15 16:01:18 +0000

    Adiciona implementação base para o modelo

---
+++ LearningTask-5.2/Models/RelatorioDeDespesas.swift
@@ -0,0 +1,20 @@
+//
+//  RelatorioDeDespesas.swift
+//  LearningTask-5.2
+//
+//  Created by rafael.rollo on 15/03/2022.
+//
+
+import Foundation
+
+struct Despesas {
+    private(set) var itens: [Despesa] = []
+    
+    var total: Decimal {
+        return itens.map({ $0.valor }).reduce(0, +)
+    }
+    
+    mutating func adiciona(_ item: Despesa) {
+        itens.append(item)
+    }
+}
