19a7bc349242254d235220f8e7a35b37c529955a
Rafael Rollo <rafaelrollo92@gmail.com>
2022-03-18 14:10:35 +0000

    Adiciona implementação final para o modelo

--- LearningTask-5.2/Models/RelatorioDeDespesas.swift
+++ LearningTask-5.2/Models/RelatorioDeDespesas.swift
@@ -7,14 +7,46 @@
 
 import Foundation
 
+struct RelatorioDeDespesas {
+    let dataDeCriacao: Date = .now
+    private(set) var despesas: Despesas = Despesas()
+    private(set) var valorTotal: Decimal = 0
+    
+    mutating func adiciona(_ despesa: Despesa) {
+        despesas.adiciona(despesa)
+        valorTotal = despesas.total
+    }
+}
+
 struct Despesas {
     private(set) var itens: [Despesa] = []
-    
+
     var total: Decimal {
         return itens.map({ $0.valor }).reduce(0, +)
     }
-    
+
     mutating func adiciona(_ item: Despesa) {
         itens.append(item)
+    }
+}
+
+struct Despesa {
+    let titulo: String
+    let tipo: Tipo
+    let valor: Decimal
+    
+    enum Tipo: Int {
+        case eletronicos = 1
+        case escritorio = 2
+    
+        var text: String {
+            switch self {
+            case .eletronicos:
+                return "Eletrônicos"
+                
+            case .escritorio:
+                return "Escritório"
+            }
+        }
     }
 }
